18d4ec92a0405ec24f26bfe1e5b25ec1d8450424
KehindeObanla <43660810+KehindeObanla@users.noreply.github.com>
2021-03-24 15:41:33 -0500

    Update client.py

--- client.py
+++ client.py
@@ -37,11 +37,7 @@ def Main(timpqp=0.8):
         # here it would be a reverse of sent message 
         print('Received from the server :',str(data.decode('ascii'))) 
         # ask the client whether he wants to continue 
-        ans = input('\nDo you want to continue(y/n) :') 
-        if ans == 'y': 
-            continue
-        else: 
-            break
+        
     # close the connection 
     s.close() 
   
